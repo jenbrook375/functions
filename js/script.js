2581aacf54d8f185260c5f3e00caf0dba19cd340
JenBrook <jenbrook375@hotmail.com>
2019-05-06 19:43:26 -0700

    learning about function definitions: declarations, expressions & constructors. parameters arguments and localstorage

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// declare a function (name parameters)
+function formatName(firstName, lastName){
+    console.log(firstName);
+    console.log(lastName);
+    // console.log(lastName + ' ' + firstName); 
+   // return lastName + ' ' + firstName;
+   var formattedName = lastName + ' ' + firstName;
+
+   // TODO: check if username isalready saved in localStorage, if it is, then don't overwrite, if not then save it
+   if (!localStorage.getItem('username')) {
+
+   } else {
+   localStorage.setItem('username', formattedName); // save to local storage
+   }
+
+   return formattedName;
+}
+
+// arguments
+var userFirstName = 'Jennifer';
+var userLastName = 'Brookshire';
+
+// call the function (pass in arguments)
+formatName(userFirstName, userLastName);
+
+// function expression:
+var createUserObj = function (firstName, lastName) {
+    var userObj = {
+    name: formatName(firstName, lastName),
+        age: null
+    };
+    return userObj; // add last set tof parenthesis to evalueate expression immediately after creation function expressions are usually only used once
+}
+
+
+// constructor
+var concatinate = new Function('x', 'y', 'return x + y' );
